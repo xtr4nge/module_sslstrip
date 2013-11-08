b20ab1b00ab73e122a1d2d59c39e60700622686e
xtr4nge <xtr4nge@gmail.com>
2013-11-08 10:41:20 +0000

    Update _info_.php

--- _info_.php
+++ _info_.php
@@ -1,6 +1,6 @@
 <?
 $mod_name="sslstrip";
-$mod_version="1.2";
+$mod_version="1.3";
 $mod_logs="/usr/share/FruityWifi/logs/sslstrip.log"; 
 $mod_logs_history="/usr/share/FruityWifi/www/modules/sslstrip/includes/logs/";
 $mod_sslstrip_inject=1;
